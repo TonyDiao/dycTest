1e7d5328d01318a6ae261e526694d43afdc1f086
yongcheng.diao <yongcheng.diao@CDTEMP16.symbio.com>
2018-07-09 17:32:12 +0800

    项目测试

---
+++ src/main/java/xin/diaoyc/dycTest/App.java
@@ -0,0 +1,13 @@
+package xin.diaoyc.dycTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
